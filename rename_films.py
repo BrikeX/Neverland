94c8cfdde449af0a3dfdd5f4cbdf09d6cffb7966
BrikeX <gqxfk1997@126.com>
2024-11-13 02:15:49 +0800

    Upgrade rename_films

--- rename_films.py
+++ rename_films.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import os
 import argparse
 
@@ -5,17 +7,31 @@ import argparse
 import exiftool
 
 
+parser = argparse.ArgumentParser(
+    description="Process films in a directory and rename them by their date and time"
+)
+parser.add_argument(
+    "-d", "--directory", type=str, required=True, help="the directory to process"
+)
+parser.add_argument("--debug", action="store_true", help="debug mode")
+args = parser.parse_args()
+
+
 def get_photo_datetime(photo_path):
     date_time = None
 
     with exiftool.ExifToolHelper() as et:
+        if args.debug:
+            print("--- photo metadata ---")
+            for metadata in et.get_metadata(photo_path):
+                for k, v in metadata.items():
+                    print(f"{k} = {v}")
+            print("--- photo metadata ---")
+
         for metadata in et.get_metadata(photo_path):
-            for k, v in metadata.items():
-                # print(f"{k} = {v}")
-                if k == "EXIF:DateTimeOriginal":
-                    date_time = v
-                    # print(f"{date_time}")
-                    break
+            if "EXIF:DateTimeOriginal" in metadata:
+                date_time = metadata.get("EXIF:DateTimeOriginal")
+
             if date_time is not None:
                 break
 
@@ -26,13 +42,19 @@ def get_video_datetime(video_path):
     date_time = None
 
     with exiftool.ExifToolHelper() as et:
+        if args.debug:
+            print("--- video metadata ---")
+            for metadata in et.get_metadata(video_path):
+                for k, v in metadata.items():
+                    print(f"{k} = {v}")
+            print("--- video metadata ---")
+
         for metadata in et.get_metadata(video_path):
-            for k, v in metadata.items():
-                # print(f"{k} = {v}")
-                if k == "QuickTime:CreateDate":
-                    date_time = v
-                    # print(f"{date_time}")
-                    break
+            if "QuickTime:CreationDate" in metadata:
+                date_time = metadata.get("QuickTime:CreationDate")
+            if date_time is None and "QuickTime:CreateDate" in metadata:
+                date_time = metadata.get("QuickTime:CreateDate")
+
             if date_time is not None:
                 break
 
@@ -61,31 +83,31 @@ def rename_films(directory_path):
     for root, _, files in os.walk(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
+            if args.debug:
+                print(f"Processing File = {file_path}")
 
             date_time = None
-            video_type = False
-            if file_path.lower().endswith((".jpg", ".jpeg")):
+            if file_path.lower().endswith((".jpg", ".jpeg", ".png", ".heic")):
                 date_time = get_photo_datetime(file_path)
             elif file_path.lower().endswith((".mov", ".mp4")):
                 date_time = get_video_datetime(file_path)
-                video_type = True
 
             if date_time is None:
                 print(f"File = {file_path} does not have DateTime")
                 continue
-            # print(f"File = {file_path}, DateTime = {date_time}")
+            if args.debug:
+                print(f"DateTime = {date_time}")
 
             formatted_date_time = format_datetime(date_time)
-            # print(f"File = {file_path}, formatted DateTime = {formatted_date_time}")
+            if args.debug:
+                print(f"Formatted DateTime = {formatted_date_time}")
 
             file_extension = os.path.splitext(file)[1]
-            new_base_name = (
-                f"VID_{formatted_date_time}"
-                if video_type
-                else f"IMG_{formatted_date_time}"
+            new_file_name = get_unique_filename(
+                root, formatted_date_time, file_extension
             )
-            new_file_name = get_unique_filename(root, new_base_name, file_extension)
-            # print(f"File = {file_path}, new name = {new_file_name}")
+            if args.debug:
+                print(f"New unique file name = {new_file_name}")
 
             new_file_path = os.path.join(root, new_file_name)
             os.rename(file_path, new_file_path)
@@ -93,20 +115,11 @@ def rename_films(directory_path):
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Process films in a directory and rename them by their date and time"
-    )
-    parser.add_argument(
-        "-d", "--directory", type=str, required=True, help="the directory to process"
-    )
-    args = parser.parse_args()
-    directory_path = args.directory
-
-    if not os.path.isdir(directory_path):
-        print(f"ERROR directory path {directory_path}")
+    if not os.path.isdir(args.directory):
+        print(f"ERROR directory path {args.directory}")
         return
 
-    rename_films(directory_path)
+    rename_films(args.directory)
 
 
 if __name__ == "__main__":
